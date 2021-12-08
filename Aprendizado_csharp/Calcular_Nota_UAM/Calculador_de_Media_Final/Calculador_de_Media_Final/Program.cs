097c786c779ac4fbdbb2abff3803e480ca3efd2d
Álvaro Piza <59285436+alvarotoledopiza@users.noreply.github.com>
2021-12-08 20:15:04 -0300

    Adicionando uma Calculadora de Notas

---
+++ Aprendizado_csharp/Calcular_Nota_UAM/Calculador_de_Media_Final/Calculador_de_Media_Final/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+
+namespace Calculadora
+{
+    class Program
+    {
+
+        static.void Main(string[]args)
+        {
+            double A1 = 
+
+
+
+        }
+    }
+
+}
