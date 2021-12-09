2a5f0e5002308fdb2ad80015014bbcc9597b7773
Álvaro Piza <59285436+alvarotoledopiza@users.noreply.github.com>
2021-12-08 22:08:31 -0300

    Inserindo a quinta variável - A5
    Calculadora de média

--- Aprendizado_csharp/Calcular_Nota_UAM/Calculador_de_Media_Final/Calculador_de_Media_Final/Program.cs
+++ Aprendizado_csharp/Calcular_Nota_UAM/Calculador_de_Media_Final/Calculador_de_Media_Final/Program.cs
@@ -10,9 +10,10 @@ namespace Calculadora
         static.void Main(string[]args)
         {
             int A1 = 2;
-            double A2 = 3.5;
-            double A3 = 4.2;
+            double A2 = 9.5;
+            double A3 = 8.2;
             double A4 = 5.5;
+            double A5 = 8.5;
 
 
 
